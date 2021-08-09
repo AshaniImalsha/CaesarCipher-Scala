14e154dffa2753e554f90afcd7e9bf210ec42320
Ashani <62203457+AshaniImalsha@users.noreply.github.com>
2021-08-09 17:13:25 +0530

    Add files via upload
    caesar-cipher program using map

---
+++ map.scala
@@ -0,0 +1,31 @@
+import scala.io.StdIn;
+
+object map{
+
+
+	def main(args:Array[String]){
+		
+		val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+		val E=(c:Char,key:Int,a:String)=>a((a.indexOf(c.toUpper)+key)%a.size)
+
+		val D=(c:Char,key:Int,a:String)=>a((a.indexOf(c.toUpper)-key)%a.size)
+
+		val cipher= (algo:(Char,Int,String)=>Char,s:String,key:Int,a:String)=>s.map(c=> if (c==' ') c else algo(c,key,a))
+		
+		print("Enter String :")
+		val s=scala.io.StdIn.readLine()
+		
+		print("Shift Value :")
+		val key=scala.io.StdIn.readInt()
+
+		val ct=cipher(E,s,key,alphabet)
+				
+		val dt=cipher(D,ct,key,alphabet)
+
+		println("Encrypt: "+ct)
+		println("Decrypt: "+dt)
+
+	}
+}
+
